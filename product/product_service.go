da3c05ee51740ae750719d93452d60f553af174b
mahmoud gomaa <gomaamahmoud922@gmail.com>
2020-07-19 16:57:51 -0500

    complete crud operations for the productService

--- product/product_service.go
+++ product/product_service.go
@@ -1,1 +1,25 @@
 package product
+
+type ProductService struct {
+	productRepo ProductRepository
+}
+
+func provideProductService(pr ProductRepository) ProductService {
+	return ProductService{productRepo: pr}
+}
+
+func (ps *ProductService) findAll() []Product {
+	return ps.productRepo.findAll()
+}
+
+func (ps *ProductService) findByID(id int) Product {
+	return ps.productRepo.findByID(id)
+}
+
+func (ps *ProductService) save(product Product) Product {
+	return ps.productRepo.save(product)
+}
+
+func (ps *ProductService) delete(product Product) {
+	ps.productRepo.delete(product)
+}
