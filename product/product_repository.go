3031d92dca517427852bbd16b99314c3c3b43637
mahmoud gomaa <gomaamahmoud922@gmail.com>
2020-07-19 16:20:06 -0500

    create Product Repository

---
+++ product/product_repository.go
@@ -0,0 +1,18 @@
+package product
+
+import (
+	"github.com/jinzhu/gorm"
+	_ "github.com/jinzhu/gorm/dialects/mysql"
+)
+
+type ProductRepository struct {
+	DB *gorm.DB
+}
+
+func providePeoductRepository(DB *gorm.DB) ProductRepository{
+	return ProductRepository(DB: DB)
+}
+
+func (p *ProductRepository) findAll []product{
+	products := [] product
+}
